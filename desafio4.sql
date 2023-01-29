96365a50c0a14d849731da8fc56c41b55a1f4d39
jaron morren <jaronmorren@gmail.com>
2023-01-29 20:35:40 -0300

    Req4:Created a query that shows active users with listening history starting from the year 2021

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT users.user_name AS `usuario`,
+	IF(YEAR(MAX(history.play_time)) >= 2021,
+    'Usuário ativo', 'Usuário inativo') AS status_usuario
+    FROM SpotifyClone.users
+    INNER JOIN SpotifyClone.history
+    ON users.user_id = history.user_id
+    GROUP BY users.user_name
+    ORDER BY users.user_name;
