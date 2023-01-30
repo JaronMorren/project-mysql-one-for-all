368706ffefe04cb78421487cba75693157fe7aea
jaron morren <jaronmorren@gmail.com>
2023-01-29 21:03:40 -0300

    Cleaned up and beautified code in requisite 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,8 +1,11 @@
-SELECT DISTINCT users.user_name AS `usuario`,
-	IF(YEAR(MAX(history.play_time)) >= 2021,
-    'Usuário ativo', 'Usuário inativo') AS status_usuario
-    FROM SpotifyClone.users
-    INNER JOIN SpotifyClone.history
-    ON users.user_id = history.user_id
-    GROUP BY users.user_name
-    ORDER BY users.user_name;
+SELECT
+    users.user_name AS `usuario`,
+    IF(YEAR(MAX(history.play_time)) >= 2021,
+        'Usuário ativo',
+        'Usuário inativo') AS status_usuario
+FROM
+    SpotifyClone.users
+        INNER JOIN
+    SpotifyClone.history ON users.user_id = history.user_id
+GROUP BY users.user_name
+ORDER BY users.user_name;
